665f657e1bcdad555ee3a4cd6bcb8e50091a41e5
unknown <shravanbr1999@gmail.com>
2021-04-06 11:02:58 +0530

    Registration added

---
+++ app/src/main/java/com/example/accidentdetectionsystem/Register_Activity.java
@@ -0,0 +1,14 @@
+package com.example.accidentdetectionsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_);
+    }
+}
